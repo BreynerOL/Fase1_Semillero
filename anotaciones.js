73b3c4606f5796dc9f3cfd6bbc8c33c92bb87f9a
incognyto <victoralzate61@gmail.com>
2022-03-04 10:48:58 -0500

    inserte anotaciones de apoyo para codigo de js

---
+++ anotaciones.js
@@ -0,0 +1,33 @@
+40 // numero
+false // boolean
+[1,2] // object
+{nombre : "lord"} // object
+null // object
+undefined // undefine
+
+//////////////////////////////////////////////////////
+
+var nombre = "victor"; // declarar e iniciar
+var edad; // declarar
+edad = 21; // inicializar
+edad // invocar
+
+var elementos = ["hello", 1]; // array
+
+var persona =  {
+    nombre: "victor", edad: 30
+}
+
+/////////////////////////////////////////////////////////
+
+// funciones declarativas
+
+function  SuperF(){
+  return 5;
+}
+
+// funciones Expresion
+
+var SuperF = function(){
+  return a+b;
+}
