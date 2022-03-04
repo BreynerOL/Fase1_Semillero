8a94d57dbad1decd54f4451a0a6898b23bb0ed9b
Jpospina1 <juan.ospina@olsoftware.com>
2022-03-04 17:00:04 -0500

    Se estudiaron y aplicaron promesas

---
+++ asincronismo/src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true){
+            resolve('Hey!');
+        }else{
+            reject('Whooooops');
+        }
+    });
+};
+
+somethingWillHappen()
+    .then(response=> console.log(response))
+    .catch(err => console.error(err));
+
+    const somethingWillHappen2 = () =>{
+        return new Promise((resolve, reject) =>{
+            if(true){
+                setTimeout(() =>{
+                    resolve('True');
+                }, 2000)
+            } else{
+                const eror = new Error('Whooop!');
+                reject(eror);
+            }
+        });
+    }
+
+    somethingWillHappen2()
+    .then(response=> console.log(response))
+    .catch(err => console.error(err));
+
+    Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response =>{
+        console.log('Array of results', response);
+    })
+    .catch(err => {
+        console.error(err);
+    })
