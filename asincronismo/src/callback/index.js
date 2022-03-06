8973ce96d82639548662b3bc50ff4bd3f8c89398
victor <victor.alzate@olsoftware.com>
2022-03-05 22:20:35 -0500

    se comprendio el tema callback

--- asincronismo/src/callback/index.js
+++ asincronismo/src/callback/index.js
@@ -1,3 +1,5 @@
+/*
+
 
 function restar(a,b) {
   return a-b;
@@ -9,20 +11,47 @@ function call(x,y,z) {
 
 console.log(call(2,3, restar));
 
+//////////////////////////////////////////////////////////////////////
 
-/*
-function date(callback) {
-  console.log(new Date);
 
+ //callback es un funcion que es pasada como parametro en otra funcion
+
+ function mensaje(fn) {
   setTimeout(function() {
-    let date = new Date;
-    callback(date);
-  }, 3000);
+    console.log("setTimeout es una funcion que se usa para congelar el tiempo"); // firts
+    fn(); // after
+  }, 3000); // tiempo que esta pausada
 }
 
-function PrintDate(datenow) {
-  console.log(datenow);
+function mensaje2() {
+  console.log("mensaje 2"); 
 }
 
-date(PrintDate);
+mensaje(mensaje2);
+
+
+setTimeout(function() {
+  console.log("this is a settimeout");
+}, 2000);
+
+
+
+///////////////////////////////////////////////////////////////
 */
+
+
+// var A = "hi";
+
+function A(fn){
+  return fn() + " mundo!";
+}
+
+function B(){ // function callback
+  return"hola"
+}
+
+function C() { // function callback
+  return "hey";
+}
+
+console.log(A(C)); // error cuando hay una variable y una funcion con el mismo nombre y se hace callback
