dea994cdebea36bff61a050c2ffc248eacc5f247
Jpospina1 <juan.ospina@olsoftware.com>
2022-03-08 15:56:35 -0500

    Se trabajo con debugger

---
+++ scope-closure/src/debug/index.js
@@ -0,0 +1,12 @@
+var a = 'Hola';
+
+function hello (){
+    let b = 'Hola mundo';
+    const c = 'Hola mundo!';
+    if (true){
+        let d= 'Hello World!!';
+        debugger
+    }
+}
+
+ hello();
