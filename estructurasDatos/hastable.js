01ead9c186131346f812999a50364236152f1769
Jpospina1 <juan.ospina@olsoftware.com>
2022-03-09 09:53:02 -0500

    Se trabajo con hansh table

---
+++ estructurasDatos/hastable.js
@@ -0,0 +1,41 @@
+class HashTable{
+    constructor(size){
+        this.data = new Array(size);
+    }
+
+    hashMethod(key){
+        let hash = 0;
+        for (let i=0; i<key.length;i++){
+hash = (hash + key.charCodeAt(i) * i)% this.data.length;
+        }
+        return hash;
+    }
+    set(key, value){
+        const address = this.hashMethod(key);
+        if(!this.data[address]){
+            this.data[address] = [];
+        }
+        this.data[address].push([key]);
+        return this.data;
+    }
+    get(key){
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if(currentBucket){
+            for(let i =0;i<currentBucket.length;i++){
+                if(currentBucket[i][0] === key){
+                    return currentBucket[i][1]; 
+                }
+            }
+        }
+        return undefined;
+    }
+}
+
+const myHashTable = new HashTable(50);
+
+myHashTable.set(1, 'Hola')
+myHashTable.set(2, 'hi')
+myHashTable.set(3, 'hello')
+
+myHashTable.get(1);
