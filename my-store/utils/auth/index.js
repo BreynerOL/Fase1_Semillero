90a7b979a43796ecdc4812e290fd9947f983baeb
Jpospina1 <juan.ospina@olsoftware.com>
2022-03-20 14:40:39 -0500

    Se han visto relaciones, consultas, listas materializadas y demas

--- my-store/utils/auth/index.js
+++
@@ -1,6 +0,0 @@
-const passport = require('passport')
-const LocalStrategy = require('./strategies/localStrategy')
-const JwtStrategy = require('./strategies/jwtStrategy')
-
-passport.use(LocalStrategy);
-passport.use(JwtStrategy);
