0e9200af947d9835d099f128908cb3425ed1d4b0
Jpospina1 <juan.ospina@olsoftware.com>
2022-04-04 11:31:55 -0500

    Se agrego el preprocesador de css sass con su configuracion

--- practico/react-shop/webpack.config.js
+++ practico/react-shop/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -27,6 +28,14 @@ module.exports = {
                         loader: 'html-loader',
                     }
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
             }
         ]
     },
@@ -34,6 +43,16 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+          },
+        compress: true,
+        port: 3005,
+      }
 }
